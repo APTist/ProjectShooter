3adeed9fbd7bbc1e510207c95c69a294bacc6c0b
APTist <rikitikitavy39@gmail.com>
2021-09-08 16:36:01 +0300

    add c++ base character class

---
+++ Source/SecondHit/SecondHit.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SecondHit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SecondHit, "SecondHit" );
